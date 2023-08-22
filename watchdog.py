1abf9a73d9407b6cc613e8a9add7552c5dd42f72
procamora <pablojose.rocamora.next@bbva.com>
2023-08-22 14:29:35 +0200

    update

--- watchdog.py
+++ watchdog.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Dict
 import sys
+import re
 
 from procamora_utils.logger import get_logging, logging
 from telebot import TeleBot
@@ -36,9 +37,13 @@ def main():
             notifications: configparser.SectionProxy = config["NOTIFICATIONS"]
 
             filter_status = list(filter(lambda e: e[1] == 'on', status.items()))
+            status_clean = list(map(lambda i: re.sub('irrigation ', '', i[0], flags=re.IGNORECASE), filter_status))
+
+            if disable_notification:  # to reduce the number of notifications
+                return
 
             bot: TeleBot = TeleBot(notifications.get('BOT_TOKEN'))
-            bot.send_message(int(notifications.get('GROUP')), f'‼️ WATCHDOG => {filter_status} ‼️',
+            bot.send_message(int(notifications.get('GROUP')), f'‼️ WATCHDOG => {status_clean} ‼️',
                              disable_notification=disable_notification)
         except Exception as err:
             log.critical(f'Error: {err}')
